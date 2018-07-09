4695aef4f543e28d8e0609f6f0714e3cf298da9c
Nikhil Parthasarathy <nikparth@gmail.com>
2018-07-09 16:50:54 -0400

    fix setup prince

--- setup_prince.sh
+++ setup_prince.sh
@@ -1,7 +1,7 @@
 #setup script for prince machines and running steerable pyramid pytorch code
 cd ~
 module purge
-module load cuda/8.0.44 pytorch/python3.6/0.3.0_4
+module load cuda/8.0.44
 
 #setup a python virtualenv
 #you can setup the virtualenv using virtualenv [name of folder for virtualenv i.e. "pytorchenv"]
@@ -9,20 +9,9 @@ virtualenv pytorchenv
 #activate virtualenv using 
 source pytorchenv/bin/activate
 #pip install the following packages
-pip install numpy, scipy, matplotlib, h5py, cffi==1.11.4, jupyter
+pip install numpy scipy matplotlib h5py jupyter torch==0.4.0 scikit-learn
 
-#clone the pytorch_fft repo and install: 
-git clone https://github.com/locuslab/pytorch_fft.git
-cd pytorch_fft 
-python setup.py install
 
-#the pytorch_fft package has some bugs right now so you need to replace the files that are created in setup
-#with the pre-built files provided in the sPyrDerNet git repo "pytorch_fft_built.zip"
-
-cd ~/sPyrDerNet/ #replace with your directory for this repo
-unzip pytorch_fft_built.zip -d ~/pytorchenv/lib/python3.6/site-packages/pytorch_fft-0.14-py3.6-linux-x86_64.egg/pytorch_fft
-
-cd ~/sPyrDerNet
 
 
 
